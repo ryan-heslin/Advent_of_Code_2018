f4e4c6933eb4e3f8bf90265274e391a9c6613b9a
ryan-heslin <rwheslin@gmail.com>
2023-04-01 15:20:30 -0400

    Day 17

--- solutions/day17.py
+++ solutions/day17.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from collections import deque
 from enum import Enum
 from itertools import starmap
 from itertools import zip_longest
@@ -11,30 +12,61 @@ class Ground(Enum):
     WATER = 2
 
 
-def flow(start, tops, source):
-    # Somehow move next block to previous space occupied
-    # Store, reverse sequence of horizontal moves?
+def flow(state, source, reservoirs):
+    # Water can flow beyond x-bounds
+    imag = attrgetter("imag")
+    sand = Ground.SAND.value
+    clay = Ground.CLAY.value
+    water = Ground.WATER.value
+    flowed = permanent = 0
 
-    # Once bottom reached, follow different behavior: flow until hitting next
-    # water tile
-    # Don't go to side if hitting pillar of water
-    # while not bottom
-        # while not blocked
-        # If above air:
-            # Move to highest space below
-                # If bottom:
-                    # bottom = True
-                    #blocked = True
-                    # break
-        # Else
-            # If can move left:
-                # Move left
-            # Else if can move right
-            # Move 1 right
-            # Else
-                # blocked = true
+    clay_veins = {k for k, v in state.items() if v == clay}
+    ymin = int(min(clay_veins, key=imag).imag)
+    ymax = int(max(clay_veins, key=imag).imag)
+    prev = deque([source])
+    done = set()
 
-    # while
+    def flow_until_obstruction(position):
+        nonlocal flowed
+        nonlocal permanent
+
+        # Always append new block, because its possible directions not yet known
+        while True:
+            if state[new := position + 1j] == sand:
+                if new.imag > ymax:
+                    done.add(position)
+                    return position
+            elif state[new] == water and new not in reservoirs:
+                done.add(position)
+                return position
+            elif state[new := position - 1] == sand:
+                pass
+            elif state[new := position + 1] == sand:
+                done.add(position)
+                # No point appending, since no other movement possible from here
+            else:
+                done.add(position)
+                return position
+
+            # Can only flow on top of flowing water
+            position = new
+            prev.appendleft(position)
+            state[position] = water
+            # Don't count beyond y bounds
+            permanent += position in reservoirs
+            flowed += ymin <= position.imag <= ymax
+
+    # If tile above water, only flow from it if in reservoir; otherwise skip
+    flow_until_obstruction(source)
+    while prev:
+        current = prev.pop()
+        if current in done:
+            continue
+        flow_until_obstruction(current)
+        prev.appendleft(current)
+
+    return flowed, permanent
+
 
 def xrange(data):
     real = attrgetter("real")
@@ -68,31 +100,76 @@ def topmost(data):
     return result
 
 
+def mark_reservoirs(xranges, underground):
+    clay = Ground.CLAY.value
+    result = set()
+
+    for xrange, floor in xranges:
+        left, right = xrange
+        y = floor - 1
+        while floor >= 0:
+            left_sealed = underground[complex(left, y)] == clay
+            right_sealed = underground[complex(right, y)] == clay
+
+            # Annoying edge case of overlapping reservoirs
+            if not (left_sealed or right_sealed):
+                break
+            if left_sealed and not right_sealed:
+                for new_right in range(left + 2, right):
+                    if underground[complex(new_right, y)] == clay:
+                        right = new_right
+                        break
+                else:
+                    break
+            elif right_sealed and not left_sealed:
+                for new_left in range(right - 2, left - 1, -1):
+                    if underground[complex(new_left, y)] == clay:
+                        left = new_left
+                        break
+                else:
+                    break
+            result.update(
+                starmap(complex, zip_longest(range(left, right + 1), (y,), fillvalue=y))
+            )
+            y -= 1
+
+    return result
+
+
 def parse(lines):
     result = defaultdict(lambda: 0)
     coords = []
+    bases = set()
 
     for line in lines:
         parts = line.split(", ")
         first = int(parts[0].split("=")[-1])
         second = tuple(map(int, parts[1][2:].split("..")))
-        pairs = list(
-            zip_longest((first,), range(second[0], second[1] + 1), fillvalue=first)
-        )
 
-        if parts[0][0] == "y":
-            pairs[0], pairs[1] = pairs[1], pairs[0]
+        if parts[0][0] == "x":
+            pairs = list(
+                zip_longest((first,), range(second[0], second[1] + 1), fillvalue=first)
+            )
+        else:
+            bases.add((second, first))
+            pairs = list(
+                zip_longest(range(second[0], second[1] + 1), (first,), fillvalue=first)
+            )
+
         coords.extend(starmap(complex, pairs))
 
     result.update(dict(zip_longest(coords, [], fillvalue=Ground.CLAY.value)))
-    return result
+    return result, bases
 
 
 with open("inputs/day17.txt") as f:
     raw_input = f.read().splitlines()
 
 source = 500
-underground = parse(raw_input)
+underground, bases = parse(raw_input)
+reservoirs = mark_reservoirs(bases, underground)
+
 ymax = int(max(underground.keys(), key=attrgetter("imag")).imag)
-# print(display(underground, ymax))
-tops = topmost(underground)
+part1, part2 = flow(underground, source, reservoirs)
+print(part1)
+print(part2)
---
+++ solutions/day20.py
@@ -0,0 +1,5 @@
+# Read pattern up to next group start/end
+# Split pattern by |
+# For each part
+# Append part to list
+# In dict, map index to indices of children
