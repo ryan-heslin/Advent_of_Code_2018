3c5a1059291a46ab30fb2135d369bc648df57064
ryan-heslin <rwheslin@gmail.com>
2023-04-03 19:10:44 -0400

    Day 12

---
+++ solutions/day12.py
@@ -0,0 +1,85 @@
+from collections import defaultdict
+
+REPLACEMENTS = {"#": True, ".": False}
+WINDOW = 5
+
+
+def false_dict():
+    return defaultdict(lambda: False)
+
+
+def parse_state(state):
+    di = false_dict()
+    di.update(dict((i, REPLACEMENTS[char]) for i, char in enumerate(state)))
+    return di
+
+
+def parse_rules(rules):
+    result = {}
+    for line in rules:
+        pattern, output = line.split(" => ")
+        result[tuple(REPLACEMENTS[char] for char in pattern)] = REPLACEMENTS[output]
+
+    return result
+
+
+def display(state):
+    return (
+        " ".join(map(str, state.keys()))
+        + "\n"
+        + " ".join("#" if v else "." for v in state.values())
+    )
+
+
+def true_sum(di):
+    return sum(k for k, v in di.items() if v)
+
+
+def simulate(state, rules, n_generations=1000):
+    edge = WINDOW // 2
+    old_difference = new_difference = part1 = None
+    # breakpoint()
+    for i in range(n_generations):
+        left = min(k for k, v in state.items() if v) - WINDOW + 1
+        middle = left + edge
+        greatest = max(state.keys())
+        new = false_dict()
+        window = [state[i] for i in range(left, left + WINDOW)]
+        if rules.get(tuple(window)):
+            new[middle] = True
+
+        while left < greatest:
+            left += 1
+            middle += 1
+            window.pop(0)
+            window.append(state[left + WINDOW - 1])
+            if rules.get(tuple(window)):
+                new[middle] = True
+
+        if i == 20:
+            part1 = true_sum(state)
+        difference = true_sum(new) - true_sum(state)
+        if difference == old_difference:
+            return part1, {
+                "difference": difference,
+                "baseline": true_sum(state),
+                "iteration": i,
+            }
+
+        state = new
+        old_difference = difference
+
+
+with open("inputs/day12.txt") as f:
+    start, rules = f.read().split("\n\n")
+
+state = parse_state(start.split(": ")[1])
+rules = rules.splitlines()
+rules = parse_rules(rules)
+
+baseline = true_sum(state)
+part1, data = simulate(state, rules)
+print(part1)
+
+part2 = data["baseline"] + (data["difference"] * (50000000000 - data["iteration"]))
+print(part2)
--- solutions/day20.py
+++ solutions/day20.py
@@ -1,5 +1,68 @@
+from collections import defaultdict
+from collections import deque
+
+GROUP_START = "("
+GROUP_END = ")"
+SEPARATOR = "|"
+
+
+def read_to_next_group(string):
+    group_start = string.find(GROUP_START)
+    group_end = string.find(GROUP_END)
+
+    # End of string
+    if group_start == -1 and group_end == -1:
+        return len(string), ""
+    elif group_start == -1 or group_end < group_start:
+        return group_end, GROUP_END
+    elif group_end == -1 or group_start < group_end:
+        return group_start, GROUP_START
+    else:
+        raise ValueError
+
+
+# If open group:
+
+
+def build_tree(pattern):
+    stop_type = None
+    parts_found = 0
+    parts = []
+    tree = defaultdict(set)
+    preceding = deque([])
+
+    while pattern != "":
+        end_char, stop_type = read_to_next_group(pattern)
+        current = pattern[:end_char]
+        pattern = pattern[end_char + 1 :]
+        parts = current.split(SEPARATOR)
+        current_preceding = preceding[-1] if len(preceding) else None
+
+        for part in parts:
+            id = parts_found
+            parts_found += 1
+            parts.append(part)
+            if preceding:
+                tree[current_preceding].add(id)
+
+        if stop_type == GROUP_START:
+            preceding.append(parts[-1])
+        elif stop_type == GROUP_END:
+            preceding.pop()
+
+    return parts, tree
+
+
 # Read pattern up to next group start/end
-# Split pattern by |
+# Split into parts
+# If parts
+# Add each part to IDs
+# Add
+# If start, append tuple of parts to stack
+# Else pop from stack
 # For each part
 # Append part to list
 # In dict, map index to indices of children
+
+# DO Dijkstra on returned reference and map
+# Doors (edges) are bidirectional
