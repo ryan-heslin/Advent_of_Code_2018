7f86dc9f75aad5780368cbd6ae2747895da2f13d
ryan-heslin <rwheslin@gmail.com>
2023-04-03 08:56:32 -0400

    Improve day 21

--- solutions/day21.py
+++ solutions/day21.py
@@ -1,8 +1,17 @@
-from collections import deque
-from math import ceil
 from math import inf
 
 from utils import timecode
+
+with open("inputs/day21.txt") as f:
+    raw_input = f.read().splitlines()
+
+loop_start = 18
+loop_end = 26
+offset = 1
+instruction_register = int(raw_input[0].split(" ")[1])
+loop_multiplier = int(raw_input[loop_start + 2].split(" ")[2])
+pointer_reset = int(raw_input[loop_end].split(" ")[1])
+exit_line = [i for i, line in enumerate(raw_input) if "0" in line.split(" ")][-1] - 1
 
 
 class CountedProgram(timecode.Program):
@@ -13,25 +22,28 @@ class CountedProgram(timecode.Program):
         states = set()
 
         new = previous = oldest = None
-        counter = 0
+        first_seen = False
+        part1 = None
+
         while 0 <= pointer < stop:
             # if pointer == 28:
             #     breakpoint()
-            counter += 1
-            if pointer == 18:
+            if pointer == loop_start:
                 self.registers[2] = 1
-                self.registers[3] = 17
-                self.registers[5] = self.registers[1] // 256
-                pointer = 26
+                self.registers[instruction_register] = pointer_reset
+                self.registers[5] = self.registers[1] // loop_multiplier
+                pointer = loop_end
             else:
-                if pointer == 28:
-                    # first = False
+                if pointer == exit_line:
                     # The one before the last new insertion - what?
                     # This doesn't make sense, and yet
                     new = self.registers[4]
                     record = (self.registers[1], new)
+                    if not first_seen:
+                        part1 = new
+                        first_seen = True
                     if record in states:
-                        return oldest
+                        return part1, oldest
                     states.add(record)
                     oldest = previous
                     # print(new)
@@ -41,30 +53,15 @@ class CountedProgram(timecode.Program):
                 self.registers[self.instruction_register] = pointer
                 self.registers = self.code[pointer](self.registers)
                 pointer = self.registers[instruction_register] + 1
-        return counter
 
-
-def find_shortest(program):
-    best = inf
-    integer = None
-    # (641049, 8164934)
-    for i in range(1000):
-        program.reset()
-        program.registers[0] = i
-        result = program.exec()
-        print(result)
-
-    return integer
-
-
-with open("inputs/day21.txt") as f:
-    raw_input = f.read().splitlines()
 
 program = CountedProgram(n_registers=6)
 program.compile(raw_input)
 
-part1 = find_shortest(program)
+program.registers[0] = None
+part1, part2 = program.exec()
 print(part1)
+print(part2)
 
 # Loop 18-25
 # Only runs once if register 0 > 256
