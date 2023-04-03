131eaae0bb5898274afe0f48bf2ba63a31338739
ryan-heslin <rwheslin@gmail.com>
2023-04-02 21:32:05 -0400

    Day 21

--- solutions/day21.py
+++ solutions/day21.py
@@ -1,3 +1,5 @@
+from collections import deque
+from math import ceil
 from math import inf
 
 from utils import timecode
@@ -5,34 +7,52 @@ from utils import timecode
 
 class CountedProgram(timecode.Program):
     def exec(self):
-        breakpoint()
-        pointer = counter = 0
+        pointer = 0
         stop = len(self.code)
         instruction_register = self.instruction_register
+        states = set()
 
+        new = previous = oldest = None
+        counter = 0
         while 0 <= pointer < stop:
-            print(pointer)
-            print(self.registers)
-            self.registers[self.instruction_register] = pointer
-            self.registers = self.code[pointer](self.registers)
-            pointer = self.registers[instruction_register] + 1
+            # if pointer == 28:
+            #     breakpoint()
             counter += 1
-
+            if pointer == 18:
+                self.registers[2] = 1
+                self.registers[3] = 17
+                self.registers[5] = self.registers[1] // 256
+                pointer = 26
+            else:
+                if pointer == 28:
+                    # first = False
+                    # The one before the last new insertion - what?
+                    # This doesn't make sense, and yet
+                    new = self.registers[4]
+                    record = (self.registers[1], new)
+                    if record in states:
+                        return oldest
+                    states.add(record)
+                    oldest = previous
+                    # print(new)
+                    # if record in states:
+                    #     return counter
+                    previous = new
+                self.registers[self.instruction_register] = pointer
+                self.registers = self.code[pointer](self.registers)
+                pointer = self.registers[instruction_register] + 1
         return counter
 
 
 def find_shortest(program):
     best = inf
     integer = None
-    for i in range(1, 1000):
-        print(i)
+    # (641049, 8164934)
+    for i in range(1000):
         program.reset()
         program.registers[0] = i
-        print(program.registers)
         result = program.exec()
-        if result < best:
-            integer = i
-            best = result
+        print(result)
 
     return integer
 
@@ -40,13 +60,19 @@ def find_shortest(program):
 with open("inputs/day21.txt") as f:
     raw_input = f.read().splitlines()
 
-program = CountedProgram(n_registers=4)
+program = CountedProgram(n_registers=6)
 program.compile(raw_input)
 
 part1 = find_shortest(program)
 print(part1)
 
 # Loop 18-25
+# Only runs once if register 0 > 256
 # 33 sets 4 to 1 if reg 0 == reg4; probable loop
 # 3 incremented by 1 on line 3,then zeroed
 # 4 zeroed
+# Register 0 read only once in program, line 31
+# Loop break line :
+# seti 27 8 3
+# 0 must be smallest number greater than whatever 4 ends up as
+# 14738375 too high
